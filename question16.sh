3f5bbcbb293a52dee1b10752b93b8f9869ebde25
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 20:14:49 +0930

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,7 +1,3 @@
-#!/bin/bash
-cd git-practice-02
 git checkout branch2
-git merge branch3 --no-edit
-git add .
-git commit -m "Merged branch3 into branch2 and resolved conflicts"
+git merge branch3
 git branch -d branch3
