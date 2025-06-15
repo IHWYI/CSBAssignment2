7ace0014d0c2a6937e8eb1ce4d51e75dedbf118b
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 20:15:16 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,26 +1,19 @@
-#!/bin/bash
-cd git-practice-03
-
-# main
-git checkout main
-mkdir -p dir1/dir2 dir3
-touch dir1/dir2/foo dir3/bar dir3/bar_copy LICENSE README.md
-git add .
-git commit -m "Setup main structure"
-
-# branch1
+# Create branch1
 git checkout -b branch1
-rm -rf dir1/dir2 dir3/bar_copy
-mkdir -p dir1
-touch dir1/foo newfile1
+rm -r dir1/dir2
+mkdir -p dir3
+touch dir3/bar
+rm dir3/bar_copy
+touch newfile1
 git add .
-git commit -m "Setup branch1 structure"
+git commit -m "Updated branch1 structure"
 
-# branch2
+# Create branch2
 git checkout main
 git checkout -b branch2
 mv dir1/dir2/foo dir1/dir2/foo_modified
 mkdir -p dir1/dir3
 touch dir1/dir3/newfile2
+rm -r dir3
 git add .
-git commit -m "Setup branch2 structure"
+git commit -m "Updated branch2 structure"
