2663736774c806cc24d1d56dc49f830eb76269d1
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 19:56:53 +0930

    Update question16.sh

--- question16.sh
+++ question16.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-git clone https://github.com/ian-knight-uofa/git-practice-02.git
 cd git-practice-02
-git checkout -b work
-git fetch origin branch2 branch3
 git checkout branch2
-git merge branch3
+git merge branch3 --no-edit
 git add .
 git commit -m "Merged branch3 into branch2 and resolved conflicts"
 git branch -d branch3
