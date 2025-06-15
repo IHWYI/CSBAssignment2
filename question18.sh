a85c0d757b1f8bf0a5555773ea7115ff1415e04a
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 19:57:38 +0930

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-git clone https://github.com/ian-knight-uofa/git-practice-04.git
 cd git-practice-04
 
-# Fetch all remote branches
+git checkout main
 git fetch --all
 
-# Merge all ready* branches into main and delete them
-git checkout main
+# Merge all ready* branches to main and delete them
 for branch in $(git branch -r | grep origin/ready | sed 's|origin/||'); do
-  git merge $branch
+  git checkout main
+  git merge $branch --no-edit
   git branch -d $branch
 done
 
-# Update all update* branches with latest main
+# Update all update* branches
 for branch in $(git branch -r | grep origin/update | sed 's|origin/||'); do
   git checkout $branch
-  git merge main
+  git merge main --no-edit
 done
