13d0448ac49de7b3f3517c6a7a3683f63a0d8f8c
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 19:56:32 +0930

    Update question15.sh

--- question15.sh
+++ question15.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-git clone https://github.com/ian-knight-uofa/git-practice-01.git
 cd git-practice-01
-git checkout -b work
 git fetch origin branch1
-git merge origin/branch1
+git checkout main
+git merge origin/branch1 --no-edit
 git add .
 git commit -m "Merged branch1 into main and resolved conflicts"
