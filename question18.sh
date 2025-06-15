a30befe47950bcedd5e5691237eafbda94c6bd62
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 20:16:06 +0930

    Update question18.sh

--- question18.sh
+++ question18.sh
@@ -1,18 +1,15 @@
-#!/bin/bash
-cd git-practice-04
-
+git branch -a
 git checkout main
-git fetch --all
-
-# Merge all ready* branches to main and delete them
-for branch in $(git branch -r | grep origin/ready | sed 's|origin/||'); do
-  git checkout main
-  git merge $branch --no-edit
-  git branch -d $branch
+for branch in $(git branch -r | grep 'ready'); do
+    git merge $branch
 done
 
-# Update all update* branches
-for branch in $(git branch -r | grep origin/update | sed 's|origin/||'); do
-  git checkout $branch
-  git merge main --no-edit
+for branch in $(git branch -r | grep 'ready'); do
+    git branch -d ${branch#origin/}
+done
+
+for branch in $(git branch -r | grep 'update'); do
+    git checkout ${branch#origin/}
+    git merge main
+    git checkout main
 done
