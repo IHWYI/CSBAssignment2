610c9d3c46904c468a2d0ac691492b3574a23826
IHWYI <85346661+IHWYI@users.noreply.github.com>
2025-06-15 20:20:53 +0930

    Update question17.sh

--- question17.sh
+++ question17.sh
@@ -1,19 +1,18 @@
-# Create branch1
+# Branch1 setup
 git checkout -b branch1
-rm -r dir1/dir2
-mkdir -p dir3
-touch dir3/bar
+rm -rf dir1/dir2
+mv dir1/dir2/foo dir1/
 rm dir3/bar_copy
 touch newfile1
 git add .
 git commit -m "Updated branch1 structure"
 
-# Create branch2
+# Branch2 setup
 git checkout main
 git checkout -b branch2
 mv dir1/dir2/foo dir1/dir2/foo_modified
 mkdir -p dir1/dir3
 touch dir1/dir3/newfile2
-rm -r dir3
+rm -rf dir3
 git add .
 git commit -m "Updated branch2 structure"
